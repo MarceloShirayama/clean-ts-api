bc4a990e7516bb770382b84a5a8ce95acfe340f5
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-27 12:26:53 -0300

    chore: remove index, server, env and protocols files from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    // '!<rootDir>/src/main/**',
+    '!<rootDir>/**/index.ts',
+    '!<rootDir>/**/server.ts',
+    '!<rootDir>/**/env.ts',
+    '!<rootDir>/**/*protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
