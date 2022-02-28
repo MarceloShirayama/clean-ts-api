14fb27090d4f2cf522625b460d204d1c0d878967
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-28 18:09:32 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
@@ -11,6 +12,10 @@ describe('SignUpValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
     }
+
+    validations.push(
+      new CompareFieldsValidation('password', 'passwordConfirmation')
+    )
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
