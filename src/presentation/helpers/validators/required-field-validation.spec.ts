cb47062f5d261d2f1b74234e1f58912c3f67faac
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-28 21:32:07 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,11 +2,19 @@ import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
 describe('RequiredFieldValidation', () => {
-  it('Should return a MissinParamError if validation fails', () => {
+  it('Should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('field')
 
     const error = sut.validate({ name: 'any_name' })
 
     expect(error).toBeInstanceOf(MissingParamError)
+  })
+
+  it('Should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+
+    const error = sut.validate({ field: 'any_name' })
+
+    expect(error).toBeFalsy()
   })
 })
