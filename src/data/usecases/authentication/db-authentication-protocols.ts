022f2e95ca04d2afd3d15fd0a897809e9525759e
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-03-01 18:50:16 -0300

    refactor: move DbAuthentication protocols to a single file

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/models/account'
+export * from '../../../domain/usecases/authentication'
+export * from '../../protocols/criptography'
+export * from '../../protocols/db'
