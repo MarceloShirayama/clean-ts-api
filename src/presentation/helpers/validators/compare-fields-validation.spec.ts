d6b92e8958173524e5283a6a90766e36a846a564
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-02-28 21:44:24 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('CompareFields Validation', () => {
+  it('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_name',
+      fieldToCompare: 'wrong_name'
+    })
+
+    expect(error).toBeInstanceOf(InvalidParamError)
+    expect(error?.message).toEqual('Invalid param: fieldToCompare')
+  })
+})
